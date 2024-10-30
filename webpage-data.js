2e483294ee645b57782bca0729cd4c39c40880c2
Camden <camden.o.b@gmail.com>
2024-10-30 13:44:30 +1300

    Add website files

---
+++ webpage-data.js
@@ -0,0 +1,25 @@
+let locations = [
+    {id: 1, name: "Cape Kidnappers", type: 1, image_location: "images/cape.jpg", description: "Cape kidnappers is a headland that sits at the end of a 8km long peninsula which protrudes into the Pacific ocean. It is a popular spot for tourists due to it being a place of importance for these birds, as it is where the gannets breed"},
+    {id: 2, name: "Blowhard Bush", type: 1, image_location: "image/blowhard.jpg", description: ""},
+    {id: 3, name: "Shine falls", type: 1, image_location: "images/shine.png", description: ""},
+    {id: 4, name: "Big Redwoods Trail ", type: 1, image_location: "images/redwoods.png", description: ""},
+    {id: 5, name: "Rose & Shamrock", type: 2, image_location: "images/r&s.jpg", description: ""},
+    {id: 6, name: "Craggy Range", type: 2, image_location: "images/craggy.jpg", description: ""},
+    {id: 7, name: "Off the Track", type: 2, image_location: "images/track.jpg", description: ""},
+    {id: 8, name: "Brave Brewery", type: 2, image_location: "images/brave.jpg", description: ""},
+    {id: 9, name: "GoBay Bus Service", type: 3, image_location: "images/gobay.jpg", description: ""},
+    {id: 10, name: "Pegasus Car Hire", type: 3, image_location: "images/pegasus.jpg", description: ""},
+    {id: 11, name: "RaD Car Hire", type: 3, image_location: "images/RaD.jpg", description: ""},
+    {id: 12, name: "Apex Car Hire", type: 3, image_location: "images/apex.png", description: ""},
+    {id: 13, name: "Porters Hotel", type: 4, image_location: "images/porters.jpg", description: ""},
+    {id: 14, name: "The DOME", type: 4, image_location: "images/dome.jpg", description: ""},
+    {id: 15, name: "Scenic Hotel Te Pania", type: 4, image_location: "images/tepania.jpg", description: ""},
+    {id: 16, name: "Art Deco Masonic Hotel", type: 4, image_location: "images/deco.jpg", description: ""}
+]
+
+let types = [
+    {id: 1, type: "Activity"},
+    {id: 2, type: "Food and Drink"},
+    {id: 3, type: "Transportation"},
+    {id: 4, type: "Accommodation"}
+]
